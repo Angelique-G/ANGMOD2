b2961d3839e590d48902c89dbfb2ed69ce461f10
Angelique Godinez <a.n.godinez.co@gmail.com>
2014-06-17 20:37:16 -0600

    another try

--- basic.js
+++ basic.js
@@ -39,6 +39,35 @@ console.log(me.name)
 
 
 // Example 6
-for (i = 0; i < 10; i++){
+for (i = 0; i < 5; i++){
     console.log(me.name)
 }
+
+
+
+var types = {
+    string: "a string",
+    number: 42,
+    boolean: true,
+    array: [1,2,3,4],
+};
+
+var key;
+
+for(key in types) {
+    console.log(key, types[key])
+}
+
+var i;
+console.log('iterating array')
+for(i=0; i< types.array.length; i++){
+    console.log(i, types.array[i])
+}
+
+
+
+
+
+
+
+
