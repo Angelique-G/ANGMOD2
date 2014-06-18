7b7f92e90aca002ccd194ac655514a7d84816bf5
Angelique Godinez <a.n.godinez.co@gmail.com>
2014-06-17 19:05:28 -0600

    trying again

--- basic.js
+++ basic.js
@@ -2,10 +2,43 @@
  * Created by Angelique on 6/17/2014.
  */
 
-
+// Example 1
 console.log("Hi");
 
+
+// Example 2
+var hi = "Hello!"
+
+console.log (hi)
+
+
+
+// Example 3
 v("Hi!")
 function v(val){
     console.log(val)
+
+}
+
+
+// Example 4
+//     var str = "test";
+
+
+
+
+// Example 5
+
+
+var me = {
+    name : "Angelique"
+};
+
+console.log(me.name)
+
+
+
+// Example 6
+for (i = 0; i < 10; i++){
+    console.log(me.name)
 }
